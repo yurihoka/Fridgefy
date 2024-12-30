2d8af69988f2d8bf14b9d94bbc36e6820e1cebbb
Yurika Hokama <yurihoka@gmail.com>
2024-12-30 11:07:03 +0900

    [Fix] Update env file name to fix login problem

--- fridgefy/src/app/api/auth/[...nextauth]/route.ts
+++ fridgefy/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET as string,
@@ -9,7 +9,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
-}
+};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
